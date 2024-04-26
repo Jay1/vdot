ec9d268059e26a4e40967bd650006545129bb24e
Jay1 <niwatix@gmail.com>
2024-04-26 06:29:16 -0400

    catppuccin instead of tokyo

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,15 +1,38 @@
 return {
 
   {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { transparent = "true" },
+    "catppuccin",
+    opts = {
+      priority = 1000,
+      flavour = "mocha",
+      transparent_background = true,
+      default_integration = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        noice = true,
+        notifier = true,
+        which_key = true,
+        telescope = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+      },
+    },
   },
-
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "catppuccin",
     },
   },
 }
+-- {
+--   "folke/tokyonight.nvim",
+--   lazy = true,
+--   opts = { transparent = "true" },
+-- },
