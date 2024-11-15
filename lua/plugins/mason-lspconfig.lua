7f9fb14a6e41c992922a20f4d66f83b393bd489c
Jay1 <j-chiasson@outlook.com>
2024-11-15 10:55:47 -0500

    Removed Hardtime

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,23 +1,21 @@
 local opts = {
-  ensure_installed = {
-    "efm",
-    "bashls",
-    "tsserver",
-    "solidity",
-    "tailwindcss",
-    "pyright",
-    "lua_ls",
-    "emmet_ls",
-    "jsonls",
-    "clangd",
-  },
+    ensure_installed = {
+        "efm",
+        "bashls",
+        "solidity",
+        "tailwindcss",
+        "pyright",
+        "lua_ls",
+        "emmet_ls",
+        "jsonls",
+    },
 
-  automatic_installation = true,
+    automatic_installation = true,
 }
 
 return {
-  "williamboman/mason-lspconfig.nvim",
-  opts = opts,
-  event = "BufReadPre",
-  dependencies = "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    opts = opts,
+    event = "BufReadPre",
+    dependencies = "williamboman/mason.nvim",
 }
