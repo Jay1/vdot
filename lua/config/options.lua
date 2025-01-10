c559641b4e6eb57c4770fa43b45cc9bf677f5f95
Jay1 <j-chiasson@outlook.com>
2025-01-10 12:52:51 -0500

    Refresh to the option file, Removed Snack animations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,74 +1,96 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- This file is automatically loaded by plugins.config
-
 local opt = vim.opt
-local options = {
+local g = vim.g
 
-  backup = false,
-  breakindent = true, -- This enables smart indentation
-  clipboard = "unnamedplus", -- Sync with system clipboard
-  colorcolumn = "80",
-  completeopt = "menu,menuone,noselect",
-  conceallevel = 0, -- Hide * markup for bold and italic
-  cmdheight = 0, -- Give more space for displaying messages.
-  cursorline = true, -- Enable highlighting of the current line
-  errorbells = false,
-  expandtab = true, -- Use spaces instead of tabs
-  fileencoding = "utf-8",
-  formatoptions = "jcroqlnt", -- tcqj
-  grepformat = "%f:%l:%c:%m",
-  grepprg = "rg --vimgrep",
-  guicursor = "", -- Makes the cursor blink
-  hlsearch = false, -- The highlighting on search remains (true is annoying)
-  ignorecase = true, -- Ignore case
-  inccommand = "nosplit", -- preview incremental substitute
-  laststatus = 0,
-  list = true, -- Show some invisible characters (tabs...
-  ls = 0, -- Disable the command line
-  incsearch = true, -- Creates new highlight when you keep typing the searches
-  mouse = "a", -- Enable mouse mode
-  number = true, -- Print line number
-  pumblend = 10, -- Popup blend
-  pumheight = 10, -- Maximum number of entries in a popup
-  relativenumber = true, -- Relative line numbers
-  scrolloff = 4, -- Lines of context
-  sessionoptions = { "buffers", "curdir", "tabpages", "winsize" },
-  shiftround = true, -- Round indent
+-- Core Editor Settings
+local options = {
+  -- File Handling
+  backup = false, -- Disable backup files
+  swapfile = false, -- Disable swap files (better with UPS or cloud storage)
+  undofile = true, -- Enable persistent undo history
+  undolevels = 10000, -- Maximum number of changes that can be undone
+  fileencoding = "utf-8", -- Default file encoding
+
+  -- Visual
+  termguicolors = true, -- Enable true color support
+  colorcolumn = "80", -- Show line length marker at 80 characters
+  cursorline = true, -- Highlight the current line
+  number = true, -- Show line numbers
+  relativenumber = true, -- Show relative line numbers
+  signcolumn = "yes", -- Always show the sign column
+  wrap = true, -- Enable line wrapping
+  list = true, -- Show invisible characters
+  showmode = false, -- Don't show mode in command line
+  pumblend = 10, -- Popup menu transparency
+  conceallevel = 0, -- Show text normally, don't hide markup
+
+  -- Editing
+  expandtab = true, -- Convert tabs to spaces
   shiftwidth = 4, -- Size of an indent
-  showmode = false, -- Dont show mode since we have a statusline
-  sidescrolloff = 8, -- Columns of context
-  signcolumn = "yes", -- Always show the signcolumn, otherwise it would shift the text each time
-  smartcase = true, -- Don't ignore case with capitals
+  tabstop = 4, -- Number of spaces tabs count for
   smartindent = true, -- Insert indents automatically
-  spelllang = { "en" },
+  breakindent = true, -- Enable smart indentation
+  shiftround = true, -- Round indent to multiple of shiftwidth
+
+  -- Search
+  hlsearch = false, -- Don't highlight all search results
+  incsearch = true, -- Show search matches as you type
+  ignorecase = true, -- Ignore case in search patterns
+  smartcase = true, -- Override ignorecase if search contains capitals
+  grepformat = "%f:%l:%c:%m", -- Format for grep results
+  grepprg = "rg --vimgrep", -- Use ripgrep for grep command
+
+  -- Window/Buffer
   splitbelow = true, -- Put new windows below current
   splitright = true, -- Put new windows right of current
-  swapfile = false, -- Swap files are annoying if you have a UPS or working in the cloud
-  tabstop = 4, -- Number of spaces tabs count for
-  termguicolors = true, -- True color support
-  timeoutlen = 300,
-  undofile = true,
-  undolevels = 10000,
-  updatetime = 200, -- Save swap file and trigger CursorHold
-  wildmode = "longest:full,full", -- Command-line completion mode
+  scrolloff = 4, -- Minimum lines to keep above/below cursor
+  sidescrolloff = 8, -- Minimum columns to keep left/right of cursor
   winminwidth = 5, -- Minimum window width
-  wrap = true, -- Disable line wrap
+  cmdheight = 0, -- Hide command line unless needed
+  laststatus = 0, -- Disable status line
+  ls = 0, -- Disable line status
+
+  -- Completion
+  completeopt = "menu,menuone,noselect", -- Completion options
+  pumheight = 10, -- Maximum number of items in completion menu
+
+  -- Performance
+  updatetime = 200, -- Faster completion and better UX
+  timeoutlen = 300, -- Time to wait for mapped sequence to complete
+
+  -- Input
+  mouse = "a", -- Enable mouse support in all modes
+  clipboard = "unnamedplus", -- Use system clipboard
+  formatoptions = "jcroqlnt", -- Configure auto-formatting options
+
+  -- Session
+  sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }, -- Session save options
+
+  -- Language
+  spelllang = { "en" }, -- Set spell checking language
 }
 
-for k, v in pairs(options) do -- This for loop is just a fancy way to set the options
-  vim.opt[k] = v -- Allows you to set the options without typing vim.opt.
+-- Global vim variables
+local globals = {
+  markdown_recommended_style = 0, -- Disable markdown auto-indent
+  snacks_animate = false, -- Disable snacks animation
+  -- Add more globals here as needed
+}
+
+-- Apply all options
+for k, v in pairs(options) do
+  opt[k] = v
 end
 
+-- Apply all globals
+for k, v in pairs(globals) do
+  g[k] = v
+end
+
+-- Neovim 0.9+ specific settings
 if vim.fn.has("nvim-0.9.0") == 1 then
-  opt.splitkeep = "screen"
-  opt.shortmess:append({ C = true })
+  opt.splitkeep = "screen" -- Keep splits positioned when resizing
+  opt.shortmess:append({ C = true }) -- Don't show completion messages
 end
-
--- Fix markdown indentation settings
-vim.g.markdown_recommended_style = 0
-
--- Change the break column color at the 80th column
--- vim.highlight.ColorColumn.ctermbg= 4
